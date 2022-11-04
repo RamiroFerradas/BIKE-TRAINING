4d17c3ec646a94389eb262e449fda05b4eaf6522
Ramiro <ramiferra97@gmail.com>
2022-11-04 09:47:32 -0300

    loader

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -3,7 +3,7 @@ import s from "./Loader.module.css";
 
 export default function Loader() {
   return (
-    <div>
+    <div className={s.body}>
       <span className={s.loader}></span>
     </div>
   );
