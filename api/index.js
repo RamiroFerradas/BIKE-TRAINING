186e0e0d97df5aa748f9087c22a068bcb029b914
Ramiro <ramiferra97@gmail.com>
2022-11-18 12:30:09 -0300

    aca

--- api/index.js
+++ api/index.js
@@ -20,13 +20,6 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
-// Syncing all the models at once.
-// conn.sync({ force: false }).then(() => {
-//   server.listen(3001, () => {
-//     console.log("%s listening at 3001"); // eslint-disable-line no-console
-//   });
-// });
-
 conn.sync({ force: false }).then(() => {
   const PORT = process.env.DB_PORT || process.env.DB_PORT_DEP; //
   server.listen(PORT, async () => {
