5237ed3edc3b09ddbb455609dda3100305ce403b
Ramiro <ramiferra97@gmail.com>
2022-12-21 13:27:39 -0300

    alter true

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ alter: true }).then(() => {
   const PORT = process.env.PORT || 3001; //
   server.listen(PORT, async () => {
     console.log(`TODO OK !! ESCUCHANDO!! PUERTO: ${PORT}`);
